0baed61af89eab78f44e1c64a6919b13df402945
Zhou Yaoyang <shinezyy@qq.com>
2019-09-10 10:40:29 +0800

    update locale and time zone info

--- 1.install-package.sh
+++ 1.install-package.sh
@@ -1,3 +1,12 @@
 sudo apt update
 sudo apt install zsh vim tmux curl wget git python3 tree htop putty-tools sudo \
     nethogs ufw landscape-common unzip
+
+sudo apt install python3-distutils -y
+sudo apt install libsodium23 -y
+sudo apt install libsodium18 -y
+
+sudo locale-gen zh_CN.UTF-8
+sudo update-locale
+
+sudo timedatectl set-timezone Asia/Shanghai && date -u && date
