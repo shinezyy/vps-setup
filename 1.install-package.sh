01404aca999cac6511ad8cb5edeef1c8b0f2986a
Zhou Yaoyang <shinezyy@qq.com>
2020-08-06 12:07:32 +0800

    add jnettop

--- 1.install-package.sh
+++ 1.install-package.sh
@@ -1,7 +1,8 @@
 sudo apt update
 sudo apt install zsh vim tmux curl wget git python3 tree htop putty-tools sudo \
-    nethogs ufw landscape-common unzip -y
+    nethogs ufw unzip net-tools jnettop -y
 
+sudo apt install landscape-common -y
 sudo apt install python3-distutils -y
 sudo apt install libsodium23 -y
 sudo apt install libsodium18 -y
