e00fcfe0f036d565b2a5c4b48412ca2e49d27fa3
Zhou Yaoyang <shinezyy@qq.com>
2023-05-02 10:48:30 +0800

    track bbr install

---
+++ bbr.sh
@@ -0,0 +1,19 @@
+uname -r
+cat /etc/lsb-release
+sudo apt install --install-recommends linux-generic-hwe-22.04
+sleep 3
+sysctl net.ipv4.tcp_available_congestion_control
+sleep 3
+sudo modprobe tcp_bbr
+sleep 3
+echo "tcp_bbr" | sudo tee -a /etc/modules-load.d/modules.conf
+sleep 3
+echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
+sleep 3
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
+sleep 3
+sudo sysctl -p
+sleep 3
+sysctl net.ipv4.tcp_available_congestion_control
+sysctl net.ipv4.tcp_congestion_control
+lsmod | grep bbr
