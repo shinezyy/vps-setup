ad7f4f6efa12decb5cf53604d10a066f8227749e
shinezyy <shinezyy@qq.com>
2023-05-08 12:11:31 +0800

    Update 1.install-package.sh

--- 1.install-package.sh
+++ 1.install-package.sh
@@ -1,13 +1,14 @@
 sudo apt update
 sudo apt install zsh vim tmux curl wget git python3 tree htop putty-tools sudo \
-    nethogs ufw unzip net-tools jnettop neovim -y
+    nethogs ufw unzip net-tools jnettop neovim vnstat -y
+
+sudo apt install python3-distutils python-is-python3 -y
 
 sudo apt install landscape-common -y
 sudo apt install unattended-upgrades -y
-sudo apt install python3-distutils -y
+
 sudo apt install libsodium23 -y
 sudo apt install libsodium18 -y
-sudo apt install python-is-python3 -y
 
 sudo locale-gen zh_CN.UTF-8
 sudo update-locale
