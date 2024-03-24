48cc30e930cca75afc736f7bf000d8c2fe2e8c5a
Zhou Yaoyang <shinezyy@qq.com>
2024-03-24 12:54:05 +0800

    add vanilla package install

---
+++ 1.install-package-vanilla.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install zsh vim tmux curl wget git python3 tree htop putty-tools sudo \
+    nethogs ufw unzip net-tools jnettop neovim vnstat -y
+
+sudo apt install python3-distutils python-is-python3 -y
+
+sudo apt install landscape-common -y
+
+sudo locale-gen zh_CN.UTF-8
+sudo update-locale
+
+sudo timedatectl set-timezone Asia/Shanghai && date -u && date
