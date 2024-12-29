0e525947ac9ec7e1cb2f8183d509857d14b494da
Zhou Yaoyang <shinezyy@qq.com>
2024-12-29 22:28:31 +0800

    refactor vps setup

---
+++ vim_tmux.sh
@@ -0,0 +1,37 @@
+mkdir -p ~/projects
+cd ~/projects
+
+# clone
+git clone https://github.com/shinezyy/my_vim_conf.git
+export vimConf=$HOME/projects/my_vim_conf
+cd $vimConf
+
+# tmux
+git submodule update --init tmux
+cd ~
+mv ~/.tmux.conf ~/.tmux.conf_bak
+mv ~/.tmux.conf.local ~/.tmux.conf.local_bak
+ln -s $vimConf/tmux/.tmux.conf .
+ln -s $vimConf/tmux/.tmux.conf.local .
+
+# vim for light-weight editing
+mv ~/.vim ~/.vim_bak
+mv ~/.vimrc ~/.vimrc_bak
+cd ~
+ln -s $vimConf/vim .vim
+ln -s $vimConf/vimrc_py .vimrc
+cd $vimConf
+git submodule update --init vim/bundle/badwolf
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# neovim for development
+mkdir -p ~/.config/nvim/lua  # create config dir for neovim
+cd ~/.config/nvim
+mv lazy-lock.json lazy-lock.json_bak
+ln -sf $vimConf/neovim/lazy-lock.json .
+mv ./lua/config ./lua/config_bak
+ln -sf $vimConf/neovim/config ./lua/config
+mv ./lua/plugins ./lua/plugins_bak
+ln -sf $vimConf/neovim/plugins ./lua/plugins
+
--- vps_setup.sh
+++ vps_setup.sh
@@ -2,37 +2,8 @@
 mkdir -p ~/.ssh/
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
-mkdir -p ~/projects
-cd ~/projects
 
-# clone
-git clone https://github.com/shinezyy/my_vim_conf.git
-
-# vim
-export vimConf=$HOME/projects/my_vim_conf
-cd $vimConf
-git submodule update --init tmux
-cd ~
-mv ~/.tmux.conf ~/.tmux.conf_bak
-ln -s $vimConf/tmux/.tmux.conf .
-mv ~/.vim ~/.vim_bak
-mv ~/.vimrc ~/.vimrc_bak
-cd ~
-ln -s $vimConf/vim .vim
-ln -s $vimConf/vimrc_bare .vimrc
-cd $vimConf
-git submodule update --init vim/bundle/Vundle.vim
-git submodule update --init vim/bundle/badwolf
-
-mkdir -p ~/.config/nvim  # create config dir for neovim
-cd ~/.config/nvim
-ln -s $vimConf/neovim/neovimrc ./init.vim
-cd ~
-mv ~/.vim ~/.vim_bak
-ln -s $vimConf/vim .vim
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+bash ./vim_tmux_setup.sh
 
 # pip3
 sudo apt install python3-pip
