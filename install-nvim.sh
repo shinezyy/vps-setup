5192a300262f66f7b30b58f0b1aa9472281e1704
Zhou Yaoyang <shinezyy@qq.com>
2025-04-27 18:39:32 +0800

    fixed neovim link

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,13 +1,18 @@
-sudo apt install libfuse2t64
+sudo apt install libfuse2
 
 mkdir -p ~/.local/bin
 cd ~/.local/bin
 
 # from AWS which is not accessable from mainland CN
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-
-chmod u+x nvim.appimage
-mv nvim.appimage nvim
+arch=$(uname -m)
+if [ $(uname -o) = "Darwin" ]; then
+    curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-${arch}.tar.gz
+elif [ $(uname -o) = "GNU/Linux" ]; then
+    curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-${arch}.appimage
+    file_name=nvim-linux-${arch}.appimage
+    chmod u+x ${file_name}
+    mv ${file_name} nvim
+fi
 
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> ~/.zshrc
 
